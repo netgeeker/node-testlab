87e46b7cb6192144529407fa0f0b7f4175d67404
netgeeker <chamilmcom@gmail.com>
2016-11-29 23:03:19 +0530

    restructured folders

--- Lab1/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var nodemon = require('gulp-nodemon');
-
-gulp.task('start', function () {
-    var options = {
-        script: 'server.js'
-    };
-    return nodemon(options);
-});
